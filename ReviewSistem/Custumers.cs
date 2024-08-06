eee11d38007f3cb672e9c685668972c820150450
Wilksonccp <wilkson.cc@gmail.com>
2024-08-06 20:46:37 -0300

    frist commit

---
+++ ReviewSistem/Custumers.cs
@@ -0,0 +1,13 @@
+namespace ReviewSistem;
+
+public class Customers
+{
+    public string Name;
+    public int Age;
+}
+public class Addresses
+{
+    public string Street;
+    public int HouseNumber;
+    public string Neighborhood;
+}
---
+++ ReviewSistem/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace ReviewSistem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customers A = new Customers();
+            Addresses End = new Addresses();
+
+            //Solicita do usuário o seu nome
+            Console.Write("Insira o nome do usuario A:");
+            A.Name = Console.ReadLine();
+
+            //solicita so usuário a sua idade
+            Console.Write("Insira a idade do usuário A");
+            A.Age = int.Parse(Console.ReadLine());
+
+            //Solicita o endereço do susuário A
+            Console.Write("Insira o endereço do usuário A, nesse formato (rua numero bairro)");
+            string[] endereco = Console.ReadLine().Split(' ');
+            End.Street = endereco[0];
+            End.HouseNumber = int.Parse(endereco[1]);
+            End.Neighborhood = endereco[2];
+
+            string status = EhVelho(A.Age);
+            
+
+            Console.WriteLine($"Oi {A.Name}, você tem {A.Age} anos");
+            Console.WriteLine($"{A.Name} você mora na Rua{End.Street}, nº {End.HouseNumber}, no bairro {End.Neighborhood}");
+            Console.WriteLine($"{A.Name}, com base em nossa análise, decobrimos que você {status}.");
+
+            static string EhVelho(int a)
+            {
+                if(a>=35)
+                {
+                    return "é velho";
+                }
+                else
+                {
+                    return "não é velho";
+
+                }
+            }
+
+        }
+    }
+}
